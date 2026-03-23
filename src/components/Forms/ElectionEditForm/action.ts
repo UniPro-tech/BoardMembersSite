4ce5456fa162275441e11b427e1e807f259814fc
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-03-23 20:27:42 +0900

    miss

--- src/components/Forms/ElectionEditForm/action.ts
+++ src/components/Forms/ElectionEditForm/action.ts
@@ -23,15 +23,11 @@ export const updateElectionAction = async (formData: FormData) => {
     throw new Error("選挙IDが不足しています");
   }
 
-  if (!title || !startAt || !standDeadline || !endAt) {
+  if (!title || !startAt || !endAt) {
     throw new Error("必要なフィールドが不足しています");
   }
 
-  if (
-    Number.isNaN(startAt.getTime()) ||
-    Number.isNaN(standDeadline.getTime()) ||
-    Number.isNaN(endAt.getTime())
-  ) {
+  if (Number.isNaN(startAt.getTime()) || Number.isNaN(endAt.getTime())) {
     throw new Error("日付の形式が正しくありません");
   }
 
@@ -39,7 +35,7 @@ export const updateElectionAction = async (formData: FormData) => {
     throw new Error("開始日時は終了日時より前でなければなりません");
   }
 
-  if (standDeadline >= endAt) {
+  if (standDeadline && standDeadline >= endAt) {
     throw new Error("立候補締切日時は終了日時より前でなければなりません");
   }
 
