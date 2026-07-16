3e06f7168e1f62164c5b510561a087c548031c95
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-07-16 15:29:03 +0900

    fx: keyの追加
    Signed-off-by: Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>

--- apps/dashboard/src/components/layouts/breadcrumb/index.tsx
+++ apps/dashboard/src/components/layouts/breadcrumb/index.tsx
@@ -17,17 +17,24 @@ export default function Breadcrumb() {
       <BreadcrumbList>
         {breadcrumbItemData.map((data, index) => (
           <>
-            <BreadcrumbItem className="hidden md:block" key={data.path}>
+            <BreadcrumbItem
+              className="hidden md:block"
+              key={`${data.path}-bc_item`}
+            >
               {index + 1 < breadcrumbItemData.length ? (
-                <BreadcrumbLink href={data.path}>{data.label}</BreadcrumbLink>
+                <BreadcrumbLink href={data.path} key={`${data.path}-bc_link`}>
+                  {data.label}
+                </BreadcrumbLink>
               ) : (
-                <BreadcrumbPage>{data.label}</BreadcrumbPage>
+                <BreadcrumbPage key={`${data.path}-bc_label`}>
+                  {data.label}
+                </BreadcrumbPage>
               )}
             </BreadcrumbItem>
             {index + 1 < breadcrumbItemData.length && (
               <BreadcrumbSeparator
                 className="hidden md:block"
-                key={data.path}
+                key={`${data.path}-bc_sep`}
               />
             )}
           </>
