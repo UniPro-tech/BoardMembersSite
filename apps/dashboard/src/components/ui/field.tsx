7111bbca1ac997135dd29b395d87351a6f308266
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-07-17 09:00:32 +0900

    fix: 文字の大きさの変更
    Signed-off-by: Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>

--- apps/dashboard/src/components/ui/field.tsx
+++ apps/dashboard/src/components/ui/field.tsx
@@ -117,7 +117,7 @@ function FieldLabel({
 
 function FieldTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
+    <h3
       data-slot="field-label"
       className={cn(
         "flex w-fit items-center gap-2 text-sm/relaxed font-medium group-data-[disabled=true]/field:opacity-50",
